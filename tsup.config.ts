541b78131694e24dd399eae9ebaa26807095418e
CoolPlayLin <CoolPlayLin@qq.com>
2023-12-31 14:22:10 +0800

    fix: ? :bug:

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "tsup";
 export default defineConfig({
   entry: ["src/main.ts"],
   format: ["esm", "cjs"],
-  target: "es2017",
+  target: "node18",
   splitting: true,
   outDir: "dist",
   clean: true
